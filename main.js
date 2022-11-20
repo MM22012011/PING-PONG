8ac78b00779ad867cd4cd82caeaafb921ccd2c12
MM22012011 <85019183+MM22012011@users.noreply.github.com>
2022-11-20 12:41:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -25,6 +25,12 @@ var ball = {
     dy:3
 }
 
+function preLoad()
+{
+  btp = loadSound("ball_touch_paddle.wav");
+  miss = loadSound("missed.wav");
+}
+
 function setup()
 {
   var canvas =  createCanvas(700,600);
@@ -58,7 +64,7 @@ function gotPoses(results)
 function startGame()
 {
   game_status = "start";
-  document.getElementById("status").innerHTML = "Gmae Is Loaded";
+  document.getElementById("status").innerHTML = "Game Is Loaded";
 }
 
 function draw()
@@ -92,7 +98,7 @@ if (game_status == "start")
    fill(250,0,0);
     stroke(0,0,250);
     strokeWeight(0.5);
-   paddle1Y = mouseY; 
+   paddle1Y = rightWristY; 
    rect(paddle1X,paddle1Y,paddle1,paddle1Height,100);
    
    
@@ -167,11 +173,13 @@ function move(){
   if (ball.y >= paddle1Y&& ball.y <= paddle1Y + paddle1Height) {
     ball.dx = -ball.dx+0.5;
     playerscore++;
+    btp.play();
   }
   else{
     pcscore++;
     reset();
     navigator.vibrate(100);
+    miss.play();
   }
 }
 if(pcscore ==4){
@@ -182,7 +190,7 @@ if(pcscore ==4){
     stroke("white");
     textSize(25)
     text("Game Over!☹☹",width/2,height/2);
-    text("Reload The Page!",width/2,height/2+30)
+    text("Press Restart Button To Play Again :)",width/2,height/2+30)
     noLoop();
     pcscore = 0;
 }
@@ -211,4 +219,11 @@ function paddleInCanvas(){
   if(mouseY < 0){
     mouseY =0;
   }  
+}
+
+function restart()
+{
+  pcscore = 0;
+  playerscore = 0;
+  loop();
 }
